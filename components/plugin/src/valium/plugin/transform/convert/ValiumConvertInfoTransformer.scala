1680ba90e63d5c6cf6841e8feab5b60d8e9314e9
Eugene Burmako <xeno.by@gmail.com>
2014-03-12 13:58:12 +0100

    outlining the conversion transformation

--- components/plugin/src/valium/plugin/transform/convert/ValiumConvertInfoTransformer.scala
+++ components/plugin/src/valium/plugin/transform/convert/ValiumConvertInfoTransformer.scala
@@ -10,5 +10,12 @@ trait ValiumConvertInfoTransformer extends InfoTransform {
   import global._
   import definitions._
 
-  override def transformInfo(sym: Symbol, tpe: Type): Type = tpe
+  override def transformInfo(sym: Symbol, tpe: Type): Type = {
+    // need to transform:
+    // 1) def foo(x: C @value) = ??? => def foo(x$x: Int, x$y: Int) = ???
+    // 2) val x: C @value = ???      => val x$x: Int = ???; val x$y: Int = ???
+    // 3) def foo: C @value = ???    => def foo: Int = ??? (only for 1-arg value classes)
+    // 4) unbox2box(x).field         => x$field
+    tpe
+  }
 }
