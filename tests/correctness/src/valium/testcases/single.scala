a32e2d7c6f24896ec4c050acdd5a0cb7be5256ee
Eugene Burmako <xeno.by@gmail.com>
2014-03-15 14:24:25 +0100

    VS test works

---
+++ tests/correctness/src/valium/testcases/single.scala
@@ -0,0 +1,38 @@
+package valium.testcases.basic
+
+@valium
+class Point(val x: Double) {
+  override def toString: String = {
+    val p1 = this
+    s"Point($x)"
+  }
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    def identity(p1: Point): Point = {
+      val p2 = p1
+      def p3 = p2
+      println(p2)
+      println(p2.toString)
+      val p4 = identity2(p3)
+      var p5 = new Point(3.0)
+      p5 = p2
+      p2
+    }
+
+    def identity2(p1: Point): Point = {
+      case class C1(p2: Point)
+      C1(p1).p2
+    }
+
+    def identity3(p1: Point): Point = {
+      class C2(private[this] val p2: Point) { def y = p2 }
+      new C2(p1).y
+    }
+
+    println(identity(new Point(1.0)))
+    println(identity(new Point(2.0)))
+    println(identity(new Point(3.0)))
+  }
+}
