e2d4f104b214d3b685c75adab816e445c0652e71
Eugene Burmako <xeno.by@gmail.com>
2014-03-16 17:25:57 +0100

    duh it still doesn't work

--- examples/basic-valium-class.scala
+++ examples/basic-valium-class.scala
@@ -1,36 +1,36 @@
 @valium
-class Point(val x: Double, val y: Double) {
+class Point[T](val x: T) {
   override def toString: String = {
     val p1 = this
-    s"Point($x, $y)"
+    s"Point($x)"
   }
 }
 
 object Test {
   def main(args: Array[String]): Unit = {
-    def identity(p1: Point): Point = {
+    def identity(p1: Point[Double]): Point[Double] = {
       val p2 = p1
       def p3 = p2
       println(p2)
       println(p2.toString)
       val p4 = identity2(p3)
-      var p5 = new Point(3.0, 0.3)
+      var p5 = new Point(3.0)
       p5 = p2
       p2
     }
 
-    def identity2(p1: Point): Point = {
-      case class C1(p2: Point)
+    def identity2(p1: Point[Double]): Point[Double] = {
+      case class C1(p2: Point[Double])
       C1(p1).p2
     }
 
-    def identity3(p1: Point): Point = {
-      class C2(private[this] val p2: Point) { def y = p2 }
+    def identity3[T](p1: Point[T]): Point[T] = {
+      class C2[T](private[this] val p2: Point[T]) { def y = p2 }
       new C2(p1).y
     }
 
-    println(identity(new Point(1.0, 1.1)))
-    println(identity(new Point(2.0, 2.1)))
-    println(identity(new Point(3.0, 3.1)))
+    println(identity(new Point(1.0)))
+    println(identity(new Point(2.0)))
+    println(identity(new Point(3.0)))
   }
 }
