d10169b2d03ce64d09a637346a83509434e936bf
Eugene Burmako <xeno.by@gmail.com>
2014-03-16 22:48:06 +0100

    this was redundant

--- components/plugin/src/valium/plugin/transform/inject/ValiumInjectTreeTransformer.scala
+++ components/plugin/src/valium/plugin/transform/inject/ValiumInjectTreeTransformer.scala
@@ -11,9 +11,9 @@ trait ValiumInjectTreeTransformer {
 
   class TreeInjector(unit: CompilationUnit) extends TreeRewriter(unit) {
     override def rewrite(tree: Tree)(implicit state: State) = {
-      case tree @ ValDef(mods, name, tpt @ V(_), rhs) if afterInject(tree.symbol.info.isUnboxedValiumRef) =>
+      case tree @ ValDef(mods, name, tpt @ V(_), rhs) =>
         commit(treeCopy.ValDef(tree, mods, name, tpt.toUnboxedValiumRef, rhs))
-      case tree @ DefDef(mods, name, tparams, vparamss, tpt @ V(_), rhs) if !tree.symbol.isConstructor && afterInject(tree.symbol.info.finalResultType.isUnboxedValiumRef) =>
+      case tree @ DefDef(mods, name, tparams, vparamss, tpt @ V(_), rhs) if tree.symbol.info.finalResultType.isUnboxedValiumRef =>
         commit(treeCopy.DefDef(tree, mods, name, tparams, super.transformValDefss(vparamss), tpt.toUnboxedValiumRef, rhs))
     }
   }
