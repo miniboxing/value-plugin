2f9a3838f7de9a2221f3cf935ab617965c46e211
Eugene Burmako <xeno.by@gmail.com>
2014-03-16 22:38:25 +0100

    bridge missing error

--- examples/basic-valium-class.scala
+++ examples/basic-valium-class.scala
@@ -1,36 +1,29 @@
-@valium
-class Point[T](val x: T) {
-  override def toString: String = {
-    val p1 = this
-    s"Point($x)"
+@valium class C(val x1: Int)
+
+trait Foo[X] {
+  def foo(x: X, y: C): X
+}
+
+trait Bar[Y] {
+  def foo(x: C, y: Y): Y
+}
+
+trait IntForC {
+  def foo(c: C, y: C): C
+}
+
+class FooForC extends Foo[C] with Bar[C] with IntForC {
+  override def foo(x: C, y: C): C = {
+    println("it works")
+    x
   }
 }
 
 object Test {
   def main(args: Array[String]): Unit = {
-    def identity(p1: Point[Double]): Point[Double] = {
-      val p2 = p1
-      def p3 = p2
-      println(p2)
-      println(p2.toString)
-      val p4 = identity2(p3)
-      var p5 = new Point(3.0)
-      p5 = p2
-      p2
-    }
-
-    def identity2(p1: Point[Double]): Point[Double] = {
-      case class C1(p2: Point[Double])
-      C1(p1).p2
-    }
-
-    def identity3[T](p1: Point[T]): Point[T] = {
-      class C2[T](private[this] val p2: Point[T]) { def y = p2 }
-      new C2(p1).y
-    }
-
-    println(identity(new Point(1.0)))
-    println(identity(new Point(2.0)))
-    println(identity(new Point(3.0)))
+    (new FooForC: Foo[C]).foo(null, null)
+    (new FooForC: Bar[C]).foo(null, null)
+    (new FooForC: IntForC).foo(null, null)
+    (new FooForC).foo(null, null)
   }
 }
