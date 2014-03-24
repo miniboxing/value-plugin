981861daad4bd5b0a1ccdd42a5852ab2e32c377e
Vlad Ureche <vlad.ureche@gmail.com>
2014-03-24 17:54:38 +0100

    More progress

--- components/plugin/src/valium/plugin/transform/coerce/ValiumAnnotationChecker.scala
+++ components/plugin/src/valium/plugin/transform/coerce/ValiumAnnotationChecker.scala
@@ -14,12 +14,14 @@ trait ValiumAnnotationCheckers {
     /**
      *  LDL FTW -- Boil frog, boil!
      */
-    override def annotationsConform(tpe1: Type, tpe2: Type): Boolean =
+    override def annotationsConform(tpe1: Type, tpe2: Type): Boolean = {
+      assert(tpe1.withoutAnnotations <:< tpe2.withoutAnnotations)
       // we run localTyper during convert during code synthesis, and there we need `T @unboxed` and `Any` to be compatible again
       if (valiumCoercePhase != null && global.phase.id == valiumCoercePhase.id) {
         tpe1.isUnboxedValiumRef == tpe2.isUnboxedValiumRef || tpe1.isWildcard || tpe2.isWildcard
       } else {
         true
       }
+    }
   }
 }
