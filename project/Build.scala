c1f56111c7d4ab59d4683d11cdab9c5a75394e1f
Vlad Ureche <vlad.ureche@gmail.com>
2014-03-29 01:52:19 +0100

    Fix partest dep

--- project/Build.scala
+++ project/Build.scala
@@ -76,7 +76,7 @@ object ValiumBuild extends Build {
       cp
     },
     libraryDependencies ++= Seq(
-      "org.scala-lang" % "scala-partest_2.11.0-M4" % "1.0-RC1",
+      "org.scala-lang.modules" %% "scala-partest" % "1.0.0",
       "com.googlecode.java-diff-utils" % "diffutils" % "1.2.1"
     )
   )
